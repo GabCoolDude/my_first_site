72330ce60cf517f43b278085f3cbffbab70cd901
GabCoolGuy <gabrielfreville@proton.me>
2023-12-29 16:17:18 +0100

    Arrays

--- Front-end/JavaScript/main.js
+++ Front-end/JavaScript/main.js
@@ -1,7 +1,15 @@
 /* Few important things : with JS, you will want to use the dev tools of your browser,
 press F12 on google for dev tools, then click on console (this is basically the only tool used )
 
-Use MDN (mozilla developer network) for JS documentation
+Use MDN (mozilla developer network) for JS documentation :
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+*/
+
+// single line comment
+
+/*multi
+line
+comment
 */
 
 console.log("Hi"); //puts "Hi" in the browser console
@@ -61,9 +69,32 @@ const s = "Hello world";
 console.log(s.length); // logs the length of the string
 console.log(s.toUpperCase()); // puts all the contents of the string to upper case
 console.log(s.toLowerCase()); // puts all the contents of the string to lower case
-console.log(s.substring(0, 5)); //logs only the specified letters (0 being the first letter)
+console.log(s.substring(0, 5)); //logs only the specified letters (0 being the first letter AND NOT 1)
 console.log(s.substring(0, 5).toUpperCase()); // you can stack these
 console.log(s.split("")); // splits every letter into one array
 
 const tags = "technology, computer, coding, tutorial";
 console.log(tags.split(","));
+
+/* Arrays : variables that hold multiples values
+Array documentation : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array */
+
+const numbers = new Array("1,2,3,4,5"); // old way
+console.log(numbers);
+const fruits = ["apples, bananas, lemons"]; // new way
+console.log(fruits);
+
+const array = ["apple", 10, true]; // you can have multiple data types in an array
+console.log(array[0]); // you can print only 1 element of an array by typing in a number, the numbers start at 0 and not 1
+
+const names = ["John", "Gab", "Doe"];
+names.push("Isaac"); // adds "grapes" to the end of the array "names"
+names.unshift("Mary"); // adds "mangos" to the end of the array "names"
+console.log(names);
+
+//names.pop(); removes the last variable of an array
+
+// console.log(Array.isArray(names)) checks if something is an array, in this case yes
+// console.log(Array.isArray("John Doe")) checks if something is an array, in this case no
+
+console.log(names.indexOf("Gab")); // prints where a variable of an array is
